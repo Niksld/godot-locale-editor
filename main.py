6c74f6fa22a9268664b76c635d48c499375b84d1
Martin F <44138297+Niksld@users.noreply.github.com>
2024-07-19 04:08:23 +0200

    Saving CSVs works, error handling, updated language list

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from languages import languages
 
 
 locale_csv: dict = {}
+csv_path = None
 default_path = os.path.expanduser("~/Desktop")
 save_path = None
 os_type = platform_system()
 locale_languages = None
+loaded_flags = []
 app_started = False
 dpg.create_context()
 
@@ -27,18 +29,44 @@ def data_load() -> None:
         new_path = f.read()
     default_path = new_path if new_path != "" else default_path
 
+def reset() -> None:
+    global locale_csv, locale_languages, loaded_flags
+    
+    logger.debug("Removing old UI elements...")
+    for item in dpg.get_item_children("button_list")[1]:
+        dpg.delete_item(item)
+        
+    for item in dpg.get_item_children("editing_window")[1]:
+        dpg.delete_item(item)
+    
+    logger.debug("Unloading flags...")
+    for flag in loaded_flags:
+        dpg.delete_item(flag)
+    
+    dpg.set_value("string_key", "No string selected")
+    
+    locale_csv = {}
+    locale_languages = None
+    
 def load_language_flags(language_list: list) -> None:
+    global loaded_flags
     update_status("Loading flags...",0)
     logger.debug("Loading flags...")
+    
+    if language_list == None:
+        logger.error("Language list is None, did we load an empty or corrupt CSV file?")
+        return
+    
     for lang in language_list:
         try:
             width, height, channels, data = dpg.load_image("./images/flags/"+lang+".png")
+            
+            if not dpg.does_item_exist(f"flag.{lang}"):
+                with dpg.texture_registry():
+                    dpg.add_static_texture(width=width, height=height, default_value=data, tag=f"flag.{lang}")
+                    loaded_flags.append(f"flag.{lang}")
         except TypeError:
             logger.info(f"Couldn't find flag for language '{lang}'. Missing flag?")
-            
-        if not dpg.does_item_exist(f"flag.{lang}"):
-            with dpg.texture_registry():
-                dpg.add_static_texture(width=width, height=height, default_value=data, tag=f"flag.{lang}")
 
 def get_save_path() -> str:
     global os_type
@@ -74,10 +102,17 @@ def load_file(sender, app_data) -> None:
     format : {"uid": [val1, val2, val3],
             ...}
     """
-    global locale_csv, save_path, locale_languages
+    global locale_csv, save_path, locale_languages, csv_path
     
     update_status("Loading CSV file...",1)
     logger.debug("Loading CSV file")
+    
+    if csv_path != None:
+        logger.info("One file has been loaded already, resetting app...")
+        reset()
+    
+    print(app_data)
+    csv_path = app_data["file_path_name"]
     with open(app_data["file_path_name"], newline='', encoding="utf-8") as csvfile:
         spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in spamreader:
@@ -101,20 +136,41 @@ def load_file(sender, app_data) -> None:
     dpg.configure_item("saveasbutton", enabled=True)
 
 def save_file():
+    global csv_path, locale_csv
     # save the file to CSV
-    # update add_input_text label hints
+    with open(csv_path, encoding="utf-8", mode="w", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=';', quotechar='|')
+        for key, val in locale_csv.items():
+            temp = val[::-1] # invert the list, so that we can append the item string name first
+            temp.append(key)
+            # and invert again to get the correct order
+            writer.writerow(temp[::-1])
+        
     # update status
     pass
 
 def generate_buttons(csv: dict) -> None: 
     global locale_languages
     logger.debug("Creating buttons")
+    
+    found_header = False
     for i in enumerate(csv.items()):
         key = i[1][0]
         
+        if key[0] == "#":
+            logger.debug("Found comment in file, skipping...")
+            continue
+        
         if key == "keys":
-            locale_languages = key = i[1][1]
+            locale_languages = i[1][1]
             logger.debug("Found the header, saving language list, skipping...")
+            found_header = True
+            continue
+        
+        if not found_header:
+            locale_languages = i[1][1]
+            logger.debug("Got first non-comment line. This should be our header.")
+            found_header = True
             continue
         
         if "[img]" in key:
@@ -128,11 +184,16 @@ def generate_input_fields():
     global languages
     
     logger.debug("Creating input fields ...")
+    
+    if locale_languages == None:
+        logger.error("Locale header is none when generating input fields, cant generate...")
+        return
+    
     for lang in enumerate(locale_languages):
         if os.path.exists(f"./images/flags/{locale_languages[lang[0]]}.png"):
-            ttip_parent = dpg.add_image(f"flag.{locale_languages[lang[0]]}", parent="editing_window", pos=(10, lang[0]*45+25), width=30, height=20)
+            ttip_parent = dpg.add_image(f"flag.{locale_languages[lang[0]]}", parent="editing_window", pos=(10, lang[0]*45+25), width=30, height=20, tag=f"img.flag.{locale_languages[lang[0]]}")
         else:
-            ttip_parent = dpg.add_text(locale_languages[lang[0]], parent="editing_window", pos=(10, lang[0]*45+25))
+            ttip_parent = dpg.add_text(locale_languages[lang[0]], parent="editing_window", pos=(10, lang[0]*45+25), tag=f"img.flag.{locale_languages[lang[0]]}")
 
         with dpg.tooltip(ttip_parent):
             try:
@@ -197,9 +258,10 @@ with dpg.file_dialog(directory_selector=False, show=False, callback=load_file, i
 with dpg.window(label="Glee - Localization Editor", width=dpg.get_viewport_width(), height=dpg.get_viewport_height(), no_move=True, no_collapse=True, no_resize=True, no_title_bar=True) as main_window:
     with dpg.menu_bar():
         with dpg.menu(label="File"):
-            dpg.add_menu_item(label="Save", enabled=False, tag="savebutton")
+            dpg.add_menu_item(label="Save", enabled=False, tag="savebutton", callback=save_file)
             dpg.add_menu_item(label="Save as..", enabled=False, tag="saveasbutton")
             dpg.add_menu_item(label="Load locale .csv", callback=lambda: dpg.show_item("file_dialog"))
+            dpg.add_menu_item(label="Close CSV File", callback=lambda: (reset(), update_status("No CSV file loaded",1)))
             dpg.add_menu_item(label="Exit") # TODO: Check if file is saved, if not prompt user to save
         with dpg.menu(label="Options"):
             dpg.add_menu_item(label="Preferences")
@@ -211,7 +273,7 @@ with dpg.window(label="Glee - Localization Editor", width=dpg.get_viewport_width
     dpg.add_child_window(width=300,height=dpg.get_viewport_height()-115, pos=(0,50), tag="button_list")
     dpg.add_child_window(width=670,height=dpg.get_viewport_height()-115, pos=(305,50), tag="editing_window")
 app_started = True
-update_status("No .CSV file loaded", 1)
+update_status("No CSV file loaded", 1)
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
